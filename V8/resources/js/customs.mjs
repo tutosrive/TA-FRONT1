e9ed24fbf1b930461a0017f0ba7400fd64e00ea2
Tutos Rive <108991712+tutosrivegamerLQ@users.noreply.github.com>
2025-02-12 11:43:34 -0500

    Update customs.mjs

--- V8/resources/js/customs.mjs
+++ V8/resources/js/customs.mjs
@@ -30,28 +30,6 @@ export function showInfoAboutUse(msg) {
   cont < 2 ? Toast.show({ message: `Puede <span class="text-info">agregar</span> ${msg} con <span class="d-inline">${addRowButton}</span> y <span class="text-danger">eliminarlos</span> con ${deleteRowButton()}` }) : null
 }
 
-// Versión original de showInfoAboutUse (copia por si la daño...)
-/**
- * * export function showInfoAboutUse(msg = null) {
- *  const span = msg ?? 'registros'
- *  let cont // Para regular las veces que se mostrará el "Toast"
- *  const countOnLocalStorage = localStorage.getItem('alertInfoOnSearch')
-   // Verificar la existencia del contador
- *  if (!countOnLocalStorage) {
-     // Si no existe, crearlo
- *    localStorage.setItem('alertInfoOnSearch', 1)
- *    cont = countOnLocalStorage
- *  } else {
-     // Castear el valor del contador almacenado
- *    cont = parseInt(countOnLocalStorage)
-     // Contador menor a 3? seguir aumentanfo contador : null
- *    cont < 3 ? localStorage.setItem('alertInfoOnSearch', cont + 1) : null
- *  }
-   // Si el contador es menor a 3, el "Toast" se mostrará
- *  cont < 3 ? Toast.show({ message: `Puede <span class="text-info">agregar</span> ${span} con <span class="d-inline">${addRowButton}</span> y <span class="text-danger">eliminarlos</span> con ${deleteRowButton()}` }) : null
- *}
- */
-
 export const classesModal = 'position-absolute top-50 start-50 translate-middle bg-dark col-12 col-sm-10 col-md-9 col-lg-8 col-xl-7'
 
 /**
